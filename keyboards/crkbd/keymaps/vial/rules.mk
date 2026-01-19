945c2677791a6c78227dd1e1665312f72cf6b246
Franco Llamas <francollamas077@gmail.com>
2026-01-18 21:47:50 -0300

    Added keyboards folder!

---
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -0,0 +1,15 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+EXTRAFLAGS += -flto=auto
+
+MOUSEKEY_ENABLE     = no
+OLED_ENABLE         = yes
+EXTRAKEY_ENABLE     = no
+COMBO_ENABLE        = no
+
+QMK_SETTINGS        = no
+
+CAPS_WORD_ENABLE = no
+LAYER_LOCK_ENABLE = no
+REPEAT_KEY_ENABLE = no
